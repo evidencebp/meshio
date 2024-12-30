b13d71f5884eaf1c419eba1c5821d6f1ca26dd14
evidencebp <idanamit@gmail.com>
2024-12-30 13:59:13 +0200

    src\meshio\gmsh\common.py line-too-long
    Made long lines shorter

--- src/meshio/gmsh/common.py
+++ src/meshio/gmsh/common.py
@@ -169,7 +169,8 @@ def _gmsh_to_meshio_order(cell_type: str, idx: ArrayLike) -> np.ndarray:
         "hexahedron20": [
             0, 1, 2, 3, 4, 5, 6, 7, 8, 11, 13,
             9, 16, 18, 19, 17, 10, 12, 14, 15,
-        ],  # https://vtk.org/doc/release/4.2/html/classvtkQuadraticHexahedron.html and https://gmsh.info/doc/texinfo/gmsh.html#Node-ordering
+        ],  # https://vtk.org/doc/release/4.2/html/classvtkQuadraticHexahedron.html 
+            # and https://gmsh.info/doc/texinfo/gmsh.html#Node-ordering
         "hexahedron27": [
             0, 1, 2, 3, 4, 5, 6, 7, 8, 11, 13,
             9, 16, 18, 19, 17, 10, 12, 14, 15,
@@ -177,7 +178,8 @@ def _gmsh_to_meshio_order(cell_type: str, idx: ArrayLike) -> np.ndarray:
         ],
         "wedge15": [
             0, 1, 2, 3, 4, 5, 6, 9, 7, 12, 14, 13, 8, 10, 11
-        ],  # http://davis.lbl.gov/Manuals/VTK-4.5/classvtkQuadraticWedge.html and https://gmsh.info/doc/texinfo/gmsh.html#Node-ordering
+        ],  # http://davis.lbl.gov/Manuals/VTK-4.5/classvtkQuadraticWedge.html 
+            # and https://gmsh.info/doc/texinfo/gmsh.html#Node-ordering
         "pyramid13": [0, 1, 2, 3, 4, 5, 8, 10, 6, 7, 9, 11, 12],
         # fmt: on
     }
